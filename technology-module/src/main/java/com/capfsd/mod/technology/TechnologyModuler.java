2a637bc92c93de32ec210d7a8ab15fa9b42398b7
willussun <willussun@gmail.com>
2019-11-25 00:56:58 +0800

    This is for test 7

--- technology-module/src/main/java/com/capfsd/mod/technology/TechnologyModuler.java
+++ technology-module/src/main/java/com/capfsd/mod/technology/TechnologyModuler.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TechnologyModuler {
-
     public static void main(String[] args) {
+        System.out.println("This is for test");
         SpringApplication.run(TechnologyModuler.class, args);
     }
 
